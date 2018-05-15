a31258bec5d27cc272f345bb24f20f65632198ea
Nye Smith <nyedess@gmail.com>
2018-05-15 17:05:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
-function deleteFromObjectByKey(object,key){
+function destructivelyDeleteFromObjectByKey(object,key){
   delete object[key]
+  return object
 }
