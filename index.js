33714f2e6c45266e975742667e54768512f28009
Nye Smith <nyedess@gmail.com>
2018-05-15 17:10:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, key){
-  
+  var clone = Object.assign({}, object)
+  delete clone[key]
+  return clone
 }
 function destructivelyDeleteFromObjectByKey(object,key){
   delete object[key]
