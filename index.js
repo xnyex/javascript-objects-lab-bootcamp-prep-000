9c7019d1e9e1ef6c51d7ec3e16b8437cd4590a93
Nye Smith <nyedess@gmail.com>
2018-05-15 17:02:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign( {}, object, {[key] : value})
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
